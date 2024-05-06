45967b9816195bdd1430d1bca81ebe21bd9ea21e
NextCodigos <nebulanemesis376>
2024-05-06 20:26:05 +0200

    Al pulsar tecla con mayus con keypress me dice que la misma tecla tiene un numero diferente

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-window.addEventListener("keydown",teclado)
+// window.addEventListener("keyup",teclado)
+window.addEventListener("keypress",teclado)
+// // window.addEventListener("keyup",teclado)
 // window.addEventListener("keyup",teclado)
 
 function teclado(e){
-    // console.log(e.key)
+    console.log(">>"+e.keyCode)
     (e.key==="ArrowUp")
     && (e.type==="keydown")
     ? document.querySelector(".campo").style.backgroundColor="red"
