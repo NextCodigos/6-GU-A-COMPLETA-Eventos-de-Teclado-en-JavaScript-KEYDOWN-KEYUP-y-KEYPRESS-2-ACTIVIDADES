c5d0e060fd321a69823425493dd44660d7e127fd
NextCodigos <nebulanemesis376>
2024-05-06 23:12:22 +0200

    Finalizado actividad 2

---
+++ Actividad1/eventos/base_actividad2/js/index.js
@@ -0,0 +1,27 @@
+document.querySelector(".campo").addEventListener("keydown",teclado);
+let pulsaciones=0;
+actualizar();
+
+function teclado(e){
+    const TECLA=e.key;
+    (TECLA==="." || TECLA===",")
+    ? e.preventDefault()
+    : escritura(TECLA)
+
+}
+
+function escritura(valor){
+    document.querySelector(
+      ".caja"
+    ).innerHTML += `<span class="tecla">${valor}</span>`;
+
+    ++pulsaciones;
+    actualizar();
+}
+
+function actualizar(){
+    const PALABRA=(pulsaciones===1)
+    ? "tecla pulsada"
+    : "teclas pulsadas";
+    document.querySelector(".pulsaciones").innerHTML=`${pulsaciones} ${PALABRA}`;
+}
