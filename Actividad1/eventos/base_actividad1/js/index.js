5eeac85eb49845e31d51d450b68c947f26af2b27
NextCodigos <nebulanemesis376>
2024-05-06 21:56:43 +0200

    E,pezamos actividad 1 con enter enciendo reactor y tecla hacia arriba sube,falta que suba con las dos pulsadas

--- Actividad1/eventos/base_actividad1/js/index.js
+++ Actividad1/eventos/base_actividad1/js/index.js
@@ -5,3 +5,15 @@ document.querySelector(".nave").style.bottom="__px";
 -Para poder SUSTITUIR una imagen por otra
 document.querySelector(".nave").src="img/nave2.png";
 */
+window.addEventListener("keydown",teclado);
+window.addEventListener("keyup",teclado);
+const NAVE=document.querySelector(".nave");
+let y=0;
+
+function teclado(e){
+    const TECLA=e.key;
+    (TECLA==="Enter") && (e.type==="keydown")
+    ? NAVE.src="img/nave2.png"
+    : NAVE.src="img/nave1.png";
+    (TECLA==="ArrowUp") && (NAVE.style.bottom=`${y+=10}px`)
+}
