707921de192f103c09f4ac2738fb04bf8cbd90ac
NextCodigos <nebulanemesis376>
2024-05-06 20:19:20 +0200

    Al pulsar cursor arriba con keydown se pone rojo div con contenido JAB

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 window.addEventListener("keydown",teclado)
-window.addEventListener("keyup",teclado)
+// window.addEventListener("keyup",teclado)
 
 function teclado(e){
-    // console.log(e)
-    (e.key==="Enter")
+    // console.log(e.key)
+    (e.key==="ArrowUp")
     && (e.type==="keydown")
     ? document.querySelector(".campo").style.backgroundColor="red"
     : document.querySelector(".campo").style.backgroundColor=null;
